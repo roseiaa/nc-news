2059c0019787a1034631d4002bebdb019e8a1e60
roseiaa <ljrobinson268@gmail.com>
2024-11-25 13:09:48 +0000

    Task 3 - Get Topics

--- app.controller.js
+++ app.controller.js
@@ -1,7 +1,17 @@
 const endpoints = require("./endpoints.json")
+const {getTopicData} = require("./models/app.models")
 
 function getApi(req, res) {
     res.status(200).send({endpoints})   
 }
 
-module.exports = {getApi}
+function getTopics(req, res) {
+    getTopicData().then((topics) => {
+        res.status(200).send({topics})
+    })
+
+}
+
+
+
+module.exports = {getApi, getTopics}
--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const {getApi} = require("./app.controller")
+const {getApi, getTopics, customServer} = require("./app.controller");
+const { customErrorHandler } = require("./app.controller");
 
 app.get("/api", getApi)
 
+app.get("/api/topics", getTopics)
+
+app.all("/*", (req, res) => {
+    res.status(404).send({message: `Route not found`})
+})
 
 module.exports = app;
